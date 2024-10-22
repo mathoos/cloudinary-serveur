cd049cfd6b0796fcc74a7d53ce2a9a717088ba6e
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-10-22 14:57:33 +0200

    update multer folder

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,20 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 const multer = require('multer');
 
 const MIME_TYPES = {
-    'image/jpg' : 'jpg',
-    'image/jpeg' : 'jpg',
-    'image/png' : 'png',
-}
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+};
 
 const storage = multer.diskStorage({
-    destination : (req, file, callback) => {
-        callback(null, 'images')
+    destination: (req, file, callback) => {
+        const dir = path.join(__dirname, '../images');
+        
+        // Vérifie si le dossier existe, sinon le crée
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+
+        callback(null, 'images');
     },
-    filename : (req, file, callback) => {
+    filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_');
         const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' + extension);
     }
-})
+});
 
-module.exports = multer({storage}).single('image');
+module.exports = multer({ storage }).single('image');
