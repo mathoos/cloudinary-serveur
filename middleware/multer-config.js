27180795a9ec8f6fc3db850da88133d18f4129c2
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-10-22 15:54:16 +0200

    cloudinary config

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -62,11 +62,15 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+console.log('CLOUDINARY_CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY);
+console.log('CLOUDINARY_API_SECRET:', process.env.CLOUDINARY_API_SECRET);
+
 // 2. Configuration de Multer pour utiliser Cloudinary
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: 'my_folder', // Nom du dossier dans Cloudinary où les images seront stockées
+        folder: 'images',
         allowed_formats: ['jpg', 'jpeg', 'png'], // Formats autorisés
         public_id: (req, file) => file.originalname.split(' ').join('_') + Date.now(), // Génère un nom unique pour chaque fichier
     },
