0c0ec6aa3f432c1123f122b597f7d5430fb7c8c7
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2025-03-28 11:27:37 +0100

    add subtitle

--- models/Object.js
+++ models/Object.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const objectSchema = mongoose.Schema({
     title: { type: String, required: true },
+    subtitle: { type: String, required: true },
     description: { type: String, required: true },
     tag: { type: String, required: true },
     imageUrl: { type: String, required: true },
     userId: { type: String, required: true },
     publicId: { type: String, required: true },
     published: { type: Boolean, default: false },
-    createdAt: { type: Date, default: Date.now }  // test render
+    createdAt: { type: Date, default: Date.now }  
 });
 
 module.exports = mongoose.model('Object', objectSchema);
